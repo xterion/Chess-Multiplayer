e92271da6a2ca2a22f992b2e00281d942e2bc3ea
Matthias <matthias.thurow@outlook.com>
2016-08-05 18:12:17 +0200

    Logik fuer Bauern

--- Chess-Multiplayer/src/game/Bauer.java
+++ Chess-Multiplayer/src/game/Bauer.java
@@ -1,6 +1,6 @@
 package game;
 
-public class Bauer extends Figure{
+public class Bauer extends Figure {
 
 	public Bauer(int x, int y, COLOR color) {
 		super(x, y, TYPE.BAUER, color);
@@ -10,32 +10,49 @@ public class Bauer extends Figure{
 	@Override
 	public void render() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void checkFields() {
-		//  *
-		//  *   *
-		//	X   X
-		
-		switch(color){
+		// *
+		// * *
+		// X X
+
+		switch (color) {
 		case WHITE:
-			if(!Board.isOnBoard(x, y-1)) break;
-			Board.moves[x][y-1] = checkColor(x, y-1);
-			if(y == 6 && Board.field[x][5] == null) Board.moves[x][4] = Board.field[x][4] == null? true : checkColor(x, 4);
-			
+			if (!Board.isOnBoard(x, y - 1))
+				break;
+			Board.moves[x][y - 1] = checkColor(x, y - 1);
+			if (y == 6 && Board.field[x][5] == null)
+				Board.moves[x][4] = Board.field[x][4] == null ? true
+						: checkColor(x, 4);
+			if (x > 0)
+				Board.moves[x - 1][y - 1] = Board.field[x - 1][y - 1] == null ? false
+						: !checkColor(x - 1, y - 1);
+			if (x < 7)
+				Board.moves[x + 1][y - 1] = Board.field[x + 1][y - 1] == null ? false
+						: !checkColor(x + 1, y - 1);
 			break;
 		case BLACK:
-			if(!Board.isOnBoard(x, y+1)) break;
-			Board.moves[x][y+1] = checkColor(x, y+1);
-			if(y == 1 && Board.field[x][2] == null) Board.moves[x][5] = Board.field[x][5] == null? true : checkColor(x, 5);
+			if (!Board.isOnBoard(x, y + 1))
+				break;
+			Board.moves[x][y + 1] = checkColor(x, y + 1);
+			if (y == 1 && Board.field[x][2] == null)
+				Board.moves[x][3] = Board.field[x][3] == null ? true
+						: checkColor(x, 3);
+			if (x > 0)
+				Board.moves[x - 1][y + 1] = Board.field[x - 1][y + 1] == null ? false
+						: !checkColor(x - 1, y + 1);
+			if (x < 7)
+				Board.moves[x + 1][y + 1] = Board.field[x + 1][y + 1] == null ? false
+						: !checkColor(x + 1, y + 1);
 			break;
 		}
 	}
