2528504d7ee726aec7a2aa60e9bac4a140178d73
Matthias <matthias.thurow@outlook.com>
2016-08-07 13:13:29 +0200

    fixed logic for Bauern

--- Chess-Multiplayer/src/game/Bauer.java
+++ Chess-Multiplayer/src/game/Bauer.java
@@ -17,30 +17,30 @@ public class Bauer extends Figure {
 		case WHITE:
 			if (!Board.isOnBoard(x, y - 1))
 				break;
-			Board.moves[x][y - 1] = checkColor(x, y - 1);
+			Board.moves[x][y - 1] = Board.field[x][y - 1] == null;
 			if (y == 6 && Board.field[x][5] == null)
 				Board.moves[x][4] = Board.field[x][4] == null ? true
 						: checkColor(x, 4);
 			if (x > 0)
 				Board.moves[x - 1][y - 1] = Board.field[x - 1][y - 1] == null ? false
-						: !checkColor(x - 1, y - 1);
+						: checkColor(x - 1, y - 1);
 			if (x < 7)
 				Board.moves[x + 1][y - 1] = Board.field[x + 1][y - 1] == null ? false
-						: !checkColor(x + 1, y - 1);
+						: checkColor(x + 1, y - 1);
 			break;
 		case BLACK:
 			if (!Board.isOnBoard(x, y + 1))
 				break;
-			Board.moves[x][y + 1] = checkColor(x, y + 1);
+			Board.moves[x][y + 1] = Board.field[x][y + 1] == null;
 			if (y == 1 && Board.field[x][2] == null)
 				Board.moves[x][3] = Board.field[x][3] == null ? true
 						: checkColor(x, 3);
 			if (x > 0)
 				Board.moves[x - 1][y + 1] = Board.field[x - 1][y + 1] == null ? false
-						: !checkColor(x - 1, y + 1);
+						: checkColor(x - 1, y + 1);
 			if (x < 7)
 				Board.moves[x + 1][y + 1] = Board.field[x + 1][y + 1] == null ? false
-						: !checkColor(x + 1, y + 1);
+						: checkColor(x + 1, y + 1);
 			break;
 		}
 	}
